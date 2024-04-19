d35191898deaf04e7e73c7e4679b604a1d88c161
ecem topaçlıoğlu <112339403+ecemt99@users.noreply.github.com>
2024-04-19 11:22:18 +0300

    Add files via upload

---
+++ console app/a/a/aApp.swift
@@ -0,0 +1,17 @@
+//
+//  aApp.swift
+//  a
+//
+//  Created by Ecem Topaclioglu on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
